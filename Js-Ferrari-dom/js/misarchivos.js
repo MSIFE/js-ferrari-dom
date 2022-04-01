e18e93a30ee19e27dfb3eaec5247ca0578bd0622
MSIFE <mati598.sife@gmail.com>
2022-04-01 16:35:26 -0300

    html dom

---
+++ Js-Ferrari-dom/js/misarchivos.js
@@ -0,0 +1,10 @@
+
+
+const week = document.getElementById("week")
+    console.log(week.innerHTML);
+
+    const weekBonus = document.createElement("li")
+    weekBonus.innerText = "SEMANAL"
+
+    week.append(weekBonus)
+    
